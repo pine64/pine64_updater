2e9066c16713a5de9884ad1fd8ebf06267e8d6ba
Marek Kraus <gamelaster@outlook.com>
2022-03-08 10:53:26 +0100

    Minor text changes

--- lib/pages/flash_pinecil_page.dart
+++ lib/pages/flash_pinecil_page.dart
@@ -363,13 +363,15 @@ class _FlashPinecilPageState extends State<FlashPinecilPage> {
         children: [
           const Text('Instructions:'),
           const Text(
-              '0. Step: check that you do not have DC barrel jack connected.'),
+              '0. Step: Check that you do not have DC barrel jack connected.'),
           const Text(
-              '1. Step: hold down the minus (-) button (closer to usb-c side).'),
-          const Text('2. Step: plug usb-c cable into Pinecil & PC (do not release (-) button).'),
-          const Text('3. Step: wait about 10 seconds until Pinecil launches to DFU mode.'),
+              '1. Step: Hold down the minus (-) button (closer to usb-c side).'),
           const Text(
-              '4. Step: release the minus (-) button. (Screen stays black during upgrade)'),
+              '2. Step: Plug USB-C cable into Pinecil & PC (do not release (-) button).'),
+          const Text(
+              '3. Step: Wait about 10 seconds until Pinecil launches to DFU mode.'),
+          const Text(
+              '4. Step: Release the minus (-) button. (Screen stays black during upgrade)'),
           Center(
             child: Image.asset("assets/images/pinecil_flashing.png"),
           ),
